1a5e996041d0bb7658cdf0c9e9ba4f9302cda1f3
Richard Marcell <richard.marcell8888@gmail.com>
2024-04-20 01:30:28 +0700

    feat: implement create penjualan page

---
+++ src/main/java/ajungstore/Barang.java
@@ -0,0 +1,5 @@
+package ajungstore;
+
+public class Barang {
+  
+}
--- src/main/java/ajungstore/Penjualan.java
+++ src/main/java/ajungstore/Penjualan.java
@@ -6,6 +6,9 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -15,6 +18,7 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -67,6 +71,17 @@ public class Penjualan {
         contentBox.getStyleClass().add("contentBox");
         contentBox.setSpacing(10);
 
+        VBox contentHeaderBox = new VBox();
+        contentHeaderBox.getStyleClass().add("contentHeader");
+
+        Label contentHeaderTitle = new Label("Dashboard Penjualan");
+        contentHeaderTitle.getStyleClass().add("contentHeaderTitle");
+
+        Label contentHeaderDescription = new Label("Pengelolaan daftar penjualan Toko Ajung");
+        contentHeaderDescription.getStyleClass().add("contentHeaderDescription");
+
+        contentHeaderBox.getChildren().addAll(contentHeaderTitle, contentHeaderDescription);
+
         HBox quickStats = new HBox();
         quickStats.getStyleClass().add("quickStats");
         quickStats.setSpacing(10);
@@ -114,6 +129,20 @@ public class Penjualan {
 
 
         VBox tableBox = new VBox();
+        tableBox.setSpacing(10);
+        Button buttonCreate = new Button("+ Penjualan");
+        buttonCreate.getStyleClass().add("buttonCreate");
+        buttonCreate.setAlignment(Pos.CENTER);
+        buttonCreate.setTextFill(Color.WHITE);
+        buttonCreate.setMinWidth(150);
+
+        buttonCreate.setOnAction(e -> {
+            try {
+                create(indexState);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
 
         TableView table = new TableView();
         // tambahkan kolom-kolom untuk tabel penjualan (no, nama customer, status, action)
@@ -130,9 +159,9 @@ public class Penjualan {
 
         table.getColumns().addAll(colNo, colNamaCustomer, colStatus, colAction);
 
-        tableBox.getChildren().add(table);
+        tableBox.getChildren().addAll(buttonCreate, table);
 
-        contentBox.getChildren().addAll(quickStats, tableBox);
+        contentBox.getChildren().addAll(contentHeaderBox, quickStats, tableBox);
 
         borderPane.setLeft(sidebar);
         borderPane.setCenter(contentBox);
@@ -146,59 +175,6 @@ public class Penjualan {
     }
 
     public void create(Stage createStage) throws Exception {
-        BorderPane borderPane = new BorderPane();
-        String css = this.getClass().getResource("styles/createPenjualan.css").toExternalForm();
-        borderPane.getStylesheets().add(css);
-
-        GridPane header = new GridPane();
-        header.setMinHeight(80);
-        header.getStyleClass().add("header");
-
-        Label appName = new Label("AjungStore");
-        appName.setTextFill(Color.RED);
-        appName.getStyleClass().add("appName");
-
-        Label welcome = new Label("Hai, Admin");
-        welcome.getStyleClass().add("welcome");
-
-        // Set the first column to expand to take the remaining space
-        ColumnConstraints column = new ColumnConstraints();
-        column.setHgrow(Priority.ALWAYS);
-        header.getColumnConstraints().add(column);
-
-        header.setAlignment(Pos.CENTER);
-
-        header.add(appName, 0, 0); // Add AjungStore to the first column, first row
-        header.add(welcome, 1, 0); // Add Hai, Admin to the second column, first row
-
-
-
-        borderPane.setTop(header);
-
-        VBox sidebar = new VBox();
-        sidebar.setMinWidth(200);
-        sidebar.getStyleClass().add("sidebar");
-
-        // Buat item navigasi
-        Label navPenjualan = new Label("Penjualan");
-        navPenjualan.getStyleClass().add("navPenjualan");
-        Label navBarang = new Label("Barang");
-        navBarang.getStyleClass().add("navBarang");
-        Label navPelanggan = new Label("Pelanggan");
-        navPelanggan.getStyleClass().add("navPelanggan");
-
-        sidebar.getChildren().addAll(navPenjualan, navBarang, navPelanggan);
-
-        borderPane.setLeft(sidebar);
-
-        Scene scene = new Scene(borderPane, 800, 600);
-        createStage.setScene(scene);
-        createStage.setFullScreen(true);
-        createStage.setTitle("AjungStore - Create Penjualan");
-        createStage.show();
-    }
-
-    public void edit(Stage ediStage) throws Exception {
         BorderPane borderPane = new BorderPane();
         String css = this.getClass().getResource("styles/createPenjualan.css").toExternalForm();
         borderPane.getStylesheets().add(css);
@@ -240,7 +216,146 @@ public class Penjualan {
 
         sidebar.getChildren().addAll(navPenjualan, navBarang, navPelanggan);
 
+        VBox contentBox = new VBox();
+        contentBox.getStyleClass().add("contentBox");
+        contentBox.setSpacing(30);
+
+        VBox contentHeaderBox = new VBox();
+        contentHeaderBox.getStyleClass().add("contentHeader");
+
+        Label contentHeaderTitle = new Label("Tambah Penjualan");
+        contentHeaderTitle.getStyleClass().add("contentHeaderTitle");
+
+        Label contentHeaderDescription = new Label("Menambah penjualan yang terjadi di Toko Ajung");
+        contentHeaderDescription.getStyleClass().add("contentHeaderDescription");
+
+        contentHeaderBox.getChildren().addAll(contentHeaderTitle, contentHeaderDescription);
+
+        VBox formBox = new VBox();
+        formBox.getStyleClass().add("formBox");
+        formBox.setSpacing(20);
+
+        VBox primaryForm = new VBox();
+        primaryForm.setSpacing(30);
+        primaryForm.getStyleClass().add("primaryForm");
+
+        HBox nomorFakturField = new HBox();
+        nomorFakturField.setSpacing(60);
+        nomorFakturField.setAlignment(Pos.CENTER_LEFT);
+        Label nomorFakturLabel = new Label("No Faktur");
+        nomorFakturLabel.getStyleClass().add("nomorFakturLabel");
+        TextField nomorFakturInput = new TextField();
+        nomorFakturInput.getStyleClass().add("nomorFakturInput");
+        HBox.setHgrow(nomorFakturInput, Priority.ALWAYS);
+        nomorFakturField.getChildren().addAll(nomorFakturLabel, nomorFakturInput);
+
+        HBox namaPelangganField = new HBox();
+        namaPelangganField.setSpacing(15);
+        namaPelangganField.setAlignment(Pos.CENTER_LEFT);
+        Label namaPelangganLabel = new Label("Nama Pelanggan");
+        namaPelangganLabel.getStyleClass().add("namaPelangganLabel");
+        namaPelangganLabel.setMinWidth(Region.USE_PREF_SIZE); // Menentukan lebar minimum agar tidak terpotong
+        ComboBox<String> namaPelangganInput = new ComboBox<>();
+        namaPelangganInput.getItems().addAll("Andi", "Budi", "Budiman Andi");
+        namaPelangganInput.getStyleClass().add("namaPelangganInput");
+        HBox.setHgrow(namaPelangganInput, Priority.ALWAYS);
+        namaPelangganInput.prefWidthProperty().bind(primaryForm.widthProperty().subtract(120)); // 60 adalah spacing dari nomorFakturField
+        namaPelangganField.getChildren().addAll(namaPelangganLabel, namaPelangganInput);
+
+        HBox tanggalField = new HBox();
+        tanggalField.setSpacing(75);
+        tanggalField.setAlignment(Pos.CENTER_LEFT);
+        Label tanggalLabel = new Label("Tanggal");
+        tanggalLabel.getStyleClass().add("tanggalLabel");
+        tanggalLabel.setMinWidth(Region.USE_PREF_SIZE);
+        DatePicker tanggalInput = new DatePicker();
+        tanggalInput.getStyleClass().add("tanggalInput");
+        HBox.setHgrow(tanggalInput, Priority.ALWAYS);
+        tanggalInput.prefWidthProperty().bind(primaryForm.widthProperty().subtract(120)); // 60 adalah spacing dari tanggalField
+        tanggalField.getChildren().addAll(tanggalLabel, tanggalInput);
+
+
+        primaryForm.getChildren().addAll(nomorFakturField, namaPelangganField, tanggalField);
+
+        VBox secondaryForm = new VBox();
+        secondaryForm.getStyleClass().add("secondaryForm");
+
+        formBox.getChildren().addAll(primaryForm, secondaryForm);
+
+        contentBox.getChildren().addAll(contentHeaderBox, formBox);
+
         borderPane.setLeft(sidebar);
+        borderPane.setCenter(contentBox);
+
+
+        Scene scene = new Scene(borderPane, 800, 600);
+        createStage.setScene(scene);
+        createStage.setFullScreen(true);
+        createStage.setTitle("AjungStore - Create Penjualan");
+        createStage.show();
+    }
+
+    public void edit(Stage ediStage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        String css = this.getClass().getResource("styles/editPenjualan.css").toExternalForm();
+        borderPane.getStylesheets().add(css);
+
+        GridPane header = new GridPane();
+        header.setMinHeight(80);
+        header.getStyleClass().add("header");
+
+        Label appName = new Label("AjungStore");
+        appName.setTextFill(Color.RED);
+        appName.getStyleClass().add("appName");
+
+        Label welcome = new Label("Hai, Admin");
+        welcome.getStyleClass().add("welcome");
+
+        // Set the first column to expand to take the remaining space
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setHgrow(Priority.ALWAYS);
+        header.getColumnConstraints().add(column1);
+
+        header.setAlignment(Pos.CENTER);
+
+        header.add(appName, 0, 0); // Add AjungStore to the first column, first row
+        header.add(welcome, 1, 0); // Add Hai, Admin to the second column, first row
+
+        borderPane.setTop(header);
+
+        VBox sidebar = new VBox();
+        sidebar.setMinWidth(200);
+        sidebar.getStyleClass().add("sidebar");
+
+        // Buat item navigasi
+        Label navPenjualan = new Label("Penjualan");
+        navPenjualan.getStyleClass().add("navPenjualan");
+        Label navBarang = new Label("Barang");
+        navBarang.getStyleClass().add("navBarang");
+        Label navPelanggan = new Label("Pelanggan");
+        navPelanggan.getStyleClass().add("navPelanggan");
+
+        sidebar.getChildren().addAll(navPenjualan, navBarang, navPelanggan);
+
+        VBox contentBox = new VBox();
+        contentBox.getStyleClass().add("contentBox");
+        contentBox.setSpacing(10);
+
+        VBox contentHeaderBox = new VBox();
+        contentHeaderBox.getStyleClass().add("contentHeader");
+
+        Label contentHeaderTitle = new Label("Edit Penjualan");
+        contentHeaderTitle.getStyleClass().add("contentHeaderTitle");
+
+        Label contentHeaderDescription = new Label("Mengedit penjualan yang terjadi di Toko Ajung");
+        contentHeaderDescription.getStyleClass().add("contentHeaderDescription");
+
+        contentHeaderBox.getChildren().addAll(contentHeaderTitle, contentHeaderDescription);
+
+        contentBox.getChildren().addAll(contentHeaderBox);
+
+        borderPane.setLeft(sidebar);
+        borderPane.setCenter(contentBox);
 
         Scene scene = new Scene(borderPane, 800, 600);
         ediStage.setScene(scene);
